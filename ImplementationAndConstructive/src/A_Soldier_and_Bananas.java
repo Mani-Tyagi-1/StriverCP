480e0eebc9f18c7de00e6d6817fde3797e783e37
ManiTyagi <manityagi1919@gmail.com>
2024-04-25 00:36:40 +0530

    Added Questions to Striver CP sheet

---
+++ ImplementationAndConstructive/src/A_Soldier_and_Bananas.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+public class A_Soldier_and_Bananas
+{
+    static PrintWriter out=new PrintWriter((System.out));
+    public static void main(String args[])throws IOException
+    {
+    //     Reader sc=new Reader();
+    //     int t=sc.nextInt();
+    //     while(t-->0)
+    //     {
+            solve();
+    //     }
+    // out.close();
+    }
+
+    public static void solve()
+    {
+        Reader R = new Reader();
+
+        long fb = R.nextLong();
+        long dollars = R.nextLong();
+        long n = R.nextLong();
+
+        long total = 0;
+
+        for (int i = 1; i <= n; i++) {
+            total += i * fb;
+        }
+
+        if (total > dollars) {
+            System.out.println(total - dollars);
+        }
+        else
+            System.out.println(0);
+        
+        
+    }
+    
+    static class Reader 
+    { 
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        public String next()
+        {
+            while(!st.hasMoreTokens())
+            {
+                try
+                {
+                    st=new StringTokenizer(br.readLine());
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        public int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+        public long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+        public double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        public String nextLine()
+        {
+            try
+            {
+                return br.readLine();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public boolean hasNext()
+        {
+            String next=null;
+            try
+            {
+                next=br.readLine();
+            }
+            catch(Exception e)
+            {
+            }
+            if(next==null)
+            {
+                return false;
+            }
+            st=new StringTokenizer(next);
+            return true;
+        }
+    } 
+}
---
+++ ImplementationAndConstructive/src/A_Tram.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+public class A_Tram
+{
+    static PrintWriter out=new PrintWriter((System.out));
+    public static void main(String args[])throws IOException
+    {
+    //     Reader sc=new Reader();
+    //     int t=sc.nextInt();
+    //     while(t-->0)
+    //     {
+            solve();
+    //     }
+    // out.close();
+    }
+
+    public static void solve()
+    {
+        Reader R = new Reader();
+
+        int n = R.nextInt();
+
+        int rem = 0;
+        int maxi = -1;
+
+        for (int i = 0; i < n; i++) {
+            int d = R.nextInt();
+            int a = R.nextInt();
+
+            rem = rem - d;
+            rem = rem + a;
+
+            maxi = Math.max(maxi, rem);
+
+        }
+        
+        System.out.println(maxi);
+    }
+    
+    static class Reader 
+    { 
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        public String next()
+        {
+            while(!st.hasMoreTokens())
+            {
+                try
+                {
+                    st=new StringTokenizer(br.readLine());
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        public int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+        public long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+        public double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        public String nextLine()
+        {
+            try
+            {
+                return br.readLine();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public boolean hasNext()
+        {
+            String next=null;
+            try
+            {
+                next=br.readLine();
+            }
+            catch(Exception e)
+            {
+            }
+            if(next==null)
+            {
+                return false;
+            }
+            st=new StringTokenizer(next);
+            return true;
+        }
+    } 
+}
